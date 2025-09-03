e48eac0c5cee054a6dfe47bb9cff265af0f3ddcc
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-09-03 09:26:43 +0100

    fix: resolve lint error by naming the variable before exporting as default :ant:

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,4 @@
-export default {
+const config = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-leading-blank': [1, 'always'],
@@ -38,3 +38,5 @@ export default {
     ],
   },
 };
+
+export default config;
