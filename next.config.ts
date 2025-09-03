deadd9b5eafaf75f12a5180cd32c86d19bc0fb95
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-09-03 15:54:20 +0100

    chore: config redirect :zap:

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async function () {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ]
+  }
 };
 
 export default nextConfig;
