c6477ffe11088e252107b07be50a2cbdd9e21f3c
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-09-03 18:43:23 +0100

    feat: dummy data for chart and branch points :fire:

---
+++ data/chart-data.ts
@@ -0,0 +1,172 @@
+// Dummy data for Point Distribution Chart
+export const pointDistributionData = [
+  {
+    id: 1,
+    label: "Branch A",
+    value: -12800,
+    iconRingColor: "red",
+    barColor: "orange"
+  },
+  {
+    id: 2,
+    label: "Branch B",
+    value: -12800,
+    iconRingColor: "green",
+    barColor: "orange"
+  },
+  {
+    id: 3,
+    label: "Branch C",
+    value: -15300,
+    iconRingColor: "black",
+    barColor: "red"
+  },
+  {
+    id: 4,
+    label: "Branch D",
+    value: -5600,
+    iconRingColor: "blue",
+    barColor: "green"
+  },
+  {
+    id: 5,
+    label: "Branch E",
+    value: 7500,
+    iconRingColor: "orange",
+    barColor: "green"
+  },
+  {
+    id: 6,
+    label: "Branch F",
+    value: 3200,
+    iconRingColor: "red",
+    barColor: "green"
+  },
+  {
+    id: 7,
+    label: "Branch G",
+    value: -2400,
+    iconRingColor: "green",
+    barColor: "green"
+  },
+  {
+    id: 8,
+    label: "Branch H",
+    value: 10800,
+    iconRingColor: "blue",
+    barColor: "orange"
+  },
+  {
+    id: 9,
+    label: "Branch I",
+    value: 9200,
+    iconRingColor: "purple",
+    barColor: "red"
+  },
+  {
+    id: 10,
+    label: "Branch J",
+    value: -1800,
+    iconRingColor: "yellow",
+    barColor: "orange"
+  },
+  {
+    id: 11,
+    label: "Branch K",
+    value: 15600,
+    iconRingColor: "red",
+    barColor: "green"
+  },
+  {
+    id: 12,
+    label: "Branch L",
+    value: -8900,
+    iconRingColor: "blue",
+    barColor: "red"
+  },
+  {
+    id: 13,
+    label: "Branch M",
+    value: 4200,
+    iconRingColor: "green",
+    barColor: "orange"
+  },
+  {
+    id: 14,
+    label: "Branch N",
+    value: -3200,
+    iconRingColor: "black",
+    barColor: "green"
+  },
+  {
+    id: 15,
+    label: "Branch O",
+    value: 11800,
+    iconRingColor: "orange",
+    barColor: "red"
+  }
+];
+
+// Dummy data for Rewards By Branch
+export const rewardsByBranchData = [
+  {
+    id: 1,
+    branchName: "Chicken republic Ikeja Branch",
+    value: -12800,
+    indicatorColor: "red"
+  },
+  {
+    id: 2,
+    branchName: "Chicken republic Ojodu Branch",
+    value: -12800,
+    indicatorColor: "green"
+  },
+  {
+    id: 3,
+    branchName: "Chicken republic Akute Branch",
+    value: 15300,
+    indicatorColor: "black"
+  },
+  {
+    id: 4,
+    branchName: "Chicken republic Ketu Branch",
+    value: -5600,
+    indicatorColor: "blue"
+  },
+  {
+    id: 5,
+    branchName: "Chicken republic VI Branch",
+    value: 7500,
+    indicatorColor: "orange"
+  },
+  {
+    id: 6,
+    branchName: "Chicken republic Surulere Branch",
+    value: 3200,
+    indicatorColor: "red"
+  },
+  {
+    id: 7,
+    branchName: "Chicken republic Bode Branch",
+    value: -2400,
+    indicatorColor: "green"
+  },
+  {
+    id: 8,
+    branchName: "Chicken republic Yba Branch",
+    value: 10800,
+    indicatorColor: "blue"
+  },
+  {
+    id: 9,
+    branchName: "Chicken republic Lekki Branch",
+    value: 9200,
+    indicatorColor: "purple"
+  },
+  {
+    id: 10,
+    branchName: "Chicken republic Ajah Branch",
+    value: -1800,
+    indicatorColor: "yellow"
+  }
+];
