b41e12151994685776297de9d946b5e91f3d81b4
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-09-03 18:39:06 +0100

    chore: rename page :zap:

--- app/dashboard/branch-level/page.tsx
+++ app/dashboard/branch-level/page.tsx
@@ -1,3 +1,12 @@
+import BranchLevel from "@/components/branch-level";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Branch Level',
+  description: 'Branch Level',
+}
+
+
 export default function Page() {
-  return <div>Branch Level</div>;
+  return <BranchLevel />
 }
--- app/dashboard/reward-tabel/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div>Reward Tabel</div>;
-}
