65033905a88b74bb3aeafb8fcb7971ff92495a9b
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-09-03 19:40:05 +0100

    feat: complete the reward page :fire:

--- app/dashboard/reward-table/page.tsx
+++ app/dashboard/reward-table/page.tsx
@@ -1,3 +1,11 @@
+import Reward from "@/components/reward";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Reward Table",
+  description: "Redeem your reward points at participating merchants across different categories, nationwide",
+};
+
 export default function Page() {
-  return <div>Reward Table</div>;
+  return <Reward />;
 }
