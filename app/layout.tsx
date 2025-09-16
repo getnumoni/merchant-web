07a604242456be1274c6b8d945703682b070509b
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-09-16 21:55:26 +0100

    chore: suppress hydration warning :zap:

--- app/layout.tsx
+++ app/layout.tsx
@@ -125,9 +125,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={` ${numoniQuicksand.variable} ${numoniOpenSans.variable} antialiased`}
+        suppressHydrationWarning
       >
         <TanstackProvider>
           {children}
