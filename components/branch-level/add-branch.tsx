707ec33242d7a00b4e91462f028a7b59ea6a6771
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-09-18 11:08:40 +0100

    chore: remove redundant on submit props :zap:

--- components/branch-level/add-branch.tsx
+++ components/branch-level/add-branch.tsx
@@ -98,7 +98,6 @@ export default function AddBranch() {
               currentStep={currentStep}
               onPrev={handlePrev}
               onNext={() => handleNext(currentStep)}
-              onSubmit={handleSubmit}
               isLastStep={currentStep === 5}
               isPending={isPending}
             />
