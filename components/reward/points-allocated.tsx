417fd2b5e00c5742a1509f3a16916874eb58a81d
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-09-26 15:01:39 +0100

    chore: update reward page to use dynamci data form api :zap:

--- components/reward/points-allocated.tsx
+++ components/reward/points-allocated.tsx
@@ -1,11 +1,10 @@
-import EmptyPointAllocation from "./empty-point-allocation";
 import PointAllocationDashboard from "./point-allocation-dashboard";
 
 export default function PointsAllocated() {
-  const isEmpty = false;
+
   return (
     <div className="space-y-4 sm:space-y-6">
-      {isEmpty ? <EmptyPointAllocation /> : <PointAllocationDashboard />}
+      <PointAllocationDashboard />
     </div>
   );
 }
