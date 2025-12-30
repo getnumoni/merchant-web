8a77dc867c4edc8a9beb50de3a7b33fe6f9b6701
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-12-30 20:02:52 +0100

    chore: add new route :tada:

---
+++ components/dashboard/point-of-sale/index.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import EmptyState from "@/components/common/empty-state";
+import { Button } from "@/components/ui/button";
+import { ErrorState } from "@/components/ui/error-state";
+import LoadingSpinner from "@/components/ui/loading-spinner";
+import useGetAllPos from "@/hooks/query/useGetAllPos";
+import { PointOfSaleData } from "@/lib/types";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import AddPOSDialog from "./add-pos-dialog";
+import PointOfSaleTable from "./point-of-sale-table";
+
+export default function PointOfSale() {
+  const [isAddPOSOpen, setIsAddPOSOpen] = useState(false);
+  const { data, isPending, isError, error } = useGetAllPos();
+
+  if (isPending) {
+    return <LoadingSpinner size="lg" message="Loading point of sale..." />;
+  }
+
+  if (isError) {
+    return <ErrorState title="Error loading data" message={error?.message || "An error occurred while loading point of sale."} />;
+  }
+
+  console.log(data?.data?.data);
+
+  const posData: PointOfSaleData[] = data?.data?.data;
+
+  if (posData?.length === 0) {
+    return <EmptyState
+      title="No Point of Sale Available"
+      description="There are currently no point of sale available. Check back later for exciting offers!"
+    />;
+  }
+
+  return <main>
+    <section className="flex justify-between items-center px-1">
+      <h1 className="text-2xl font-bold">Point of Sale History</h1>
+      <Button
+        onClick={() => setIsAddPOSOpen(true)}
+        className="bg-theme-dark-green hover:bg-theme-dark-green/90 text-white px-12 py-6 rounded-lg font-medium transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        <Plus /> Add Point of Sale
+      </Button>
+    </section>
+
+    <PointOfSaleTable data={posData} title="Point of Sale History" searchValue="" />
+
+    <AddPOSDialog
+      isOpen={isAddPOSOpen}
+      onClose={() => setIsAddPOSOpen(false)}
+    />
+  </main>;
+}
--- constant/routes.ts
+++ constant/routes.ts
@@ -8,6 +8,7 @@ const DASHBOARD_URL: string = '/dashboard';
 const BRANCH_LEVEL_URL: string = `${DASHBOARD_URL}/branch-level`;
 const REWARD_TABLE_URL: string = `${DASHBOARD_URL}/reward-table`;
 const TRANSACTIONS_URL: string = `${DASHBOARD_URL}/transactions`;
+const POS_URL: string = `${DASHBOARD_URL}/point-of-sale`;
 const POINTS_URL: string = `${DASHBOARD_URL}/points`;
 const DEALS_AND_PROMOS_URL: string = `${DASHBOARD_URL}/deals-and-promos`;
 const CHARITY_ORGS_URL: string = `${DASHBOARD_URL}/charity-orgs`;
@@ -15,5 +16,5 @@ const CHARITY_ORGS_URL: string = `${DASHBOARD_URL}/charity-orgs`;
 
 
 
-export { BRANCH_LEVEL_URL, CHARITY_ORGS_URL, DASHBOARD_URL, DEALS_AND_PROMOS_URL, POINTS_URL, REWARD_TABLE_URL, SIGN_IN_URL, SIGN_UP_URL, TRANSACTIONS_URL };
+export { BRANCH_LEVEL_URL, CHARITY_ORGS_URL, DASHBOARD_URL, DEALS_AND_PROMOS_URL, POINTS_URL, POS_URL, REWARD_TABLE_URL, SIGN_IN_URL, SIGN_UP_URL, TRANSACTIONS_URL };
 
