fb7df366f6d6d6ef68cd1ce1836ae18ee5b9a80c
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-01-21 16:05:06 +0100

    chore: update with readonly in props :zap:

--- components/dashboard/transaction/components/transaction-summary-header.tsx
+++ components/dashboard/transaction/components/transaction-summary-header.tsx
@@ -1,6 +1,11 @@
-'use client';
+"use client";
 
-import SummaryDateRangeFilter, { SummaryDateRangeOption } from "./summary-date-range-filter";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import ExportTransaction from "../export-transaction";
+import SummaryDateRangeFilter, {
+  SummaryDateRangeOption,
+} from "./summary-date-range-filter";
 
 interface TransactionSummaryHeaderProps {
   selectedRange: SummaryDateRangeOption;
@@ -10,12 +15,32 @@ interface TransactionSummaryHeaderProps {
 export default function TransactionSummaryHeader({
   selectedRange,
   onRangeChange,
-}: TransactionSummaryHeaderProps) {
+}: Readonly<TransactionSummaryHeaderProps>) {
+  const [openExportModal, setOpenExportModal] = useState<boolean>(false);
+
   return (
     <div className="flex items-center justify-between my-4">
-      <h2 className="text-lg font-semibold text-gray-900">Transaction Summary</h2>
-      <SummaryDateRangeFilter selectedRange={selectedRange} onRangeChange={onRangeChange} />
+      <h2 className="text-lg font-semibold text-gray-900">
+        Transaction Summary
+      </h2>
+      <div className="flex gap-2">
+        <div>
+          <Button
+            className="bg-theme-dark-green"
+            onClick={() => setOpenExportModal(true)}
+          >
+            Export
+          </Button>
+        </div>
+        <SummaryDateRangeFilter
+          selectedRange={selectedRange}
+          onRangeChange={onRangeChange}
+        />
+      </div>
+      <ExportTransaction
+        isOpen={openExportModal}
+        onClose={() => setOpenExportModal(false)}
+      />
     </div>
   );
 }
-
